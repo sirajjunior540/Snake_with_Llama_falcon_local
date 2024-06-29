244421d7f4fbc800519191b6734edd0cf3b2199a
abdullah <sirajjunior@gmail.com>
2024-06-29 14:00:37 +0400

    adding the weight

--- agent.py
+++ agent.py
@@ -13,8 +13,8 @@ MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
-model_file = './model/model.pth'
-optimizer_file = './model/optimizer.pth'
+model_file = 'model.pth'
+optimizer_file = 'model/optimizer.pth'
 
 
 class Agent:
